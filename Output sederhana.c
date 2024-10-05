7e32dc6b22f3222da756c88e02cb534d5c17857c
DevaniPramitaDewi <devanipramita@gmail.com>
2024-10-05 16:11:24 +0800

    Belajar C sederhana

---
+++ Output sederhana.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Halo semua!\n");
+    printf("Perkenalkan aku Ni Luh Putu Devani Pramita Dewi\n");
+    printf("Aku mahasiswa Universitas Udayana\n");
+    printf("Fakultas Teknik dengan Jurusan Teknologi Informasi\n");
+    printf("Senang mengenal kalian :)\n");
+    return 0;
+}
---
+++ Penjumlahan sederhana.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,hasil;
+ 
+    printf("Masukkan angka : ");
+    scanf("%d",&a);
+    printf("Masukkan angka : ");
+    scanf("%d",&b);
+ 
+    hasil=a + b;
+    printf("Hasilnya = %d",hasil);
+    return 0;
+}
